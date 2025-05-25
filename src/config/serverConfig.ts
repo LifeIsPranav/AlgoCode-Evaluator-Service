e3a46cbc23bb70842531270cdb47f2114369de0c
Pranav <lifeispranav@gmail.com>
2025-05-25 23:18:17 +0530

    Basic ioredis Setup with building done

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -4,4 +4,10 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 3000,
+  REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
+  REDIS_PORT: Number(process.env.REDIS_PORT) || 3000,
 };
+
+// export const PORT = process.env.PORT || 3000;
+// export const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+// export const REDIS_PORT = process.env.REDIS_PORT || '6379';
